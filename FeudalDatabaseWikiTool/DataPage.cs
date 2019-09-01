db106b014f9c3650cc67b593ca8e47b38522d445
Deantwo <Deantwo@users.noreply.github.com>
2019-09-01 13:13:30 +0200

    Fixed SkillData output
    Fixed a missing colon in the SkillData output.

--- FeudalDatabaseWikiTool/DataPage.cs
+++ FeudalDatabaseWikiTool/DataPage.cs
@@ -60,7 +60,7 @@ namespace FeudalDatabaseWikiTool
                 sb.AppendLine($"\t\tDescLvl100 = {JsonFormat(feudalSkill.DescLvl100)},");
                 sb.AppendLine($"\t\tMasterMessageID = {JsonFormat(feudalSkill.MasterMessageID)},");
                 sb.AppendLine($"\t\tGMMessageID = {JsonFormat(feudalSkill.GMMessageID)},");
-                sb.AppendLine($"\t\tskill_mult = {JsonFormat(feudalSkill.skill_mult)}");
+                sb.AppendLine($"\t\tskill_mult = {JsonFormat(feudalSkill.skill_mult)},");
                 sb.AppendLine($"\t\tExpToSkillMult = {JsonFormat(feudalSkill.ExpToSkillMult)}");
             }
 
